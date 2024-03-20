251b73ca3b6b0542723cacb2074722e94f0794d7
Evgeniya Troshina <troshina450@gmail.com>
2024-03-20 11:12:51 +0300

    5.js added

---
+++ 5.js
@@ -0,0 +1,16 @@
+// Реализовать функцию round5, которой передается целое число, функция должна
+// возвращать ближайшее целое число к тому что было передано, без остатка делящееся
+// на 5.
+// console.log(round5(0)); // 0
+// console.log(round5(2)); // 0
+// console.log(round5(3)); // 5
+// console.log(round5(11)); // 10
+// console.log(round5(14)); // 15
+// console.log(round5(50)); // 50
+// console.log(round5(-2)); // 0
+// console.log(round5(-3)); // -5
+
+function round5(num) {
+  return Math.round(num / 5) * 5;
+}
+console.log(round5(14));
